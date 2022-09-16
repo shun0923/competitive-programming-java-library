ef5b6d5d9ed917f4badc338d9263b07e02a2eb9c
shun0923 <shuny0923@gmail.com>
2022-09-16 17:40:01 +0900

    Create ManyFastIO_test.java

---
+++ test/ManyFastIO_test.java
@@ -0,0 +1,17 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/many_aplusb
+package test;
+
+import library.Solver;
+
+public class ManyFastIO_Test extends Solver {
+	public static void main(final String[] args) {
+		DEBUG = args.length > 0 && args[0].equals("-DEBUG");
+		Thread.setDefaultUncaughtExceptionHandler((t, e) -> { flush(); e.printStackTrace(); System.exit(1); });
+		new Thread(null, new ManyFastIO_Test(), "", 1 << 31).start();
+	}
+
+	public void solve() {
+		int t = ni();
+		for(int i = 0; i < t; i ++) prtln(nl() + nl());
+	}
+}
