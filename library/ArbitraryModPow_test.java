28d8c143e6a5517028f786c236f2d66c4d6dec26
shun0923 <shuny0923@gmail.com>
2022-09-17 16:17:06 +0900

    Update ModPow test

--- library/ArbitraryModPow_test.java
+++ library/ArbitraryModPow_test.java
@@ -4,16 +4,16 @@ package library;
 
 import java.util.*;
 import library.Util;
-import library.Mod;
+import library.ArbitraryMod;
 
-public class Mod107Pow_test extends Util {
+public class ArbitraryModPow_test extends Util {
 	public static void main(final String[] args) {
 		DEBUG = args.length > 0 && args[0].equals("-DEBUG");
 		Thread.setDefaultUncaughtExceptionHandler((t, e) -> { flush(); e.printStackTrace(); System.exit(1); });
-		new Thread(null, new Mod107Pow_test(), "", 1 << 31).start();
+		new Thread(null, new ArbitraryModPow_test(), "", 1 << 31).start();
 	}
 
 	public void solve() {
-		prtln(Mod107.md.pow(nl(), nl()));
+		prtln(new ArbitraryMod(1_000_000_007).pow(nl(), nl()));
 	}
 }
