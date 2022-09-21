94005bc9b875fbc5e95ae86f1264c7067f55e8c4
shun0923 <shuny0923@gmail.com>
2022-09-21 20:30:17 +0900

    Remove Pair from Util

--- library/TemplateSegmentTree_Composite_test.java
+++ library/TemplateSegmentTree_Composite_test.java
@@ -3,6 +3,7 @@
 package library;
 
 import library.Solver;
+import library.Pair;
 import library.Mod;
 import library.TemplateSegmentTree;
 
@@ -13,14 +14,14 @@ public class TemplateSegmentTree_Composite_test extends Solver {
 		Mod md = Mod998.md;
 		int n = ni();
 		int q = ni();
-		PairLL f[] = npll(n);
-		TemplateSegmentTree<PairLL> st = new TemplateSegmentTree<>(f,
-			() -> new PairLL(1, 0),
-			(ele1, ele2) -> new PairLL(md.mul(ele1.a, ele2.a), md.mod(ele2.a * ele1.b + ele2.b)));
+		Pair.LL f[] = npll(n);
+		TemplateSegmentTree<Pair.LL> st = new TemplateSegmentTree<>(f,
+			() -> new Pair.LL(1, 0),
+			(ele1, ele2) -> new Pair.LL(md.mul(ele1.a, ele2.a), md.mod(ele2.a * ele1.b + ele2.b)));
 		for(int i = 0; i < q; i ++) {
 			if(ni() == 0) st.set(ni(), npll());
 			else {
-				PairLL p = st.find(ni(), ni());
+				Pair.LL p = st.find(ni(), ni());
 				prtln(md.mod(p.a * nl() + p.b));
 			}
 		}
