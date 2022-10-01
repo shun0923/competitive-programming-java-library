bdb2ad6568e9f8dc066c273a27fa94b2e17a7717
shun0923 <shuny0923@gmail.com>
2022-10-01 13:33:40 +0900

    Update

--- library/AbstractGraph.java
+++ library/AbstractGraph.java
@@ -6,9 +6,9 @@ import library.SimpleUtil;
 abstract class AbstractGraph<Node extends AbstractNode<Edge>, Edge extends AbstractEdge<Edge>> {
 	public int numNode;
 	public boolean directed;
-	public Node edges;
-	public Node nodes[];
-	protected Node reversedNodes[] = null;
+	private Node edges;
+	private Node nodes[];
+	private Node reversedNodes[] = null;
 
 	protected abstract Node createNode(final int id);
 	protected abstract Node[] createNodeList(final int size);
@@ -22,7 +22,10 @@ abstract class AbstractGraph<Node extends AbstractNode<Edge>, Edge extends Abstr
 		for(int i = 0; i < numNode; i ++) nodes[i] = createNode(i);
 	}
 
-	public final Node[] reverseNodes() {
+	public final int numEdge() { return edges.size(); }
+	public Node edges() { return edges; }
+	public Node[] nodes() { return nodes; }
+	public Node[] reverseNodes() {
 		if(reversedNodes == null) {
 			reversedNodes = createNodeList(numNode);
 			for(int i = 0; i < numNode; i ++) reversedNodes[i] = createNode(i);
@@ -31,7 +34,7 @@ abstract class AbstractGraph<Node extends AbstractNode<Edge>, Edge extends Abstr
 		return reversedNodes;
 	}
 
-	public final void add(final AbstractNode<? extends Edge> edges) { this.edges.addAll(edges); }
+	public final void addAll(final Collection<? extends Edge> edges) { this.edges.addAll(edges); }
 	public final void add(final Edge e) {
 		edges.add(e);
 		nodes[e.source].add(e);
@@ -48,66 +51,88 @@ abstract class AbstractGraph<Node extends AbstractNode<Edge>, Edge extends Abstr
 
 	public final void clear() { edges.clear(); for(Node n : nodes) n.clear(); reversedNodes = null; }
 }
-abstract class UnweightedGraph<Node extends AbstractNode<UnweightedEdge>> extends AbstractGraph<Node, UnweightedEdge> {
+abstract class UnweightedGraph<Node extends UnweightedNode> extends AbstractGraph<Node, UnweightedEdge> {
 	public UnweightedGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	public void add(final int source, final int target) { add(new UnweightedEdge(source, target)); }
+	@Override public Node edges() { return super.edges(); }
+	@Override public Node[] nodes() { return super.nodes(); }
+	@Override public Node[] reverseNodes() { return super.reverseNodes(); }
 }
-abstract class WeightedGraph<Node extends AbstractNode<WeightedEdge>> extends AbstractGraph<Node, WeightedEdge> {
+abstract class WeightedGraph<Node extends WeightedNode> extends AbstractGraph<Node, WeightedEdge> {
 	public WeightedGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	public void add(final int source, final int target, final long cost) { add(new WeightedEdge(source, target, cost)); }
 	public void update(final WeightedEdge e, final long cost) { remove(e); e.cost = cost; add(e); }
+	@Override public Node edges() { return super.edges(); }
+	@Override public Node[] nodes() { return super.nodes(); }
+	@Override public Node[] reverseNodes() { return super.reverseNodes(); }
 }
-abstract class TemplateGraph<T extends Comparable<T>, Node extends AbstractNode<TemplateEdge<T>>> extends AbstractGraph<Node, TemplateEdge<T>> {
+abstract class TemplateGraph<T extends Comparable<T>, Node extends TemplateNode<T>> extends AbstractGraph<Node, TemplateEdge<T>> {
 	public TemplateGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	public void add(final int source, final int target, final T cost) { add(new TemplateEdge<T>(source, target, cost)); }
 	public void update(final TemplateEdge<T> e, final T cost) { remove(e); e.cost = cost; add(e); }
+	@Override public Node edges() { return super.edges(); }
+	@Override public Node[] nodes() { return super.nodes(); }
+	@Override public Node[] reverseNodes() { return super.reverseNodes(); }
 }
 class ArrayUnweightedGraph extends UnweightedGraph<ArrayUnweightedNode> {
 	public ArrayUnweightedGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	@Override protected final ArrayUnweightedNode createNode(final int id) { return new ArrayUnweightedNode(id); }
 	@Override protected final ArrayUnweightedNode[] createNodeList(final int size) { return new ArrayUnweightedNode[size]; }
+	@Override public ArrayUnweightedNode edges() { return super.edges(); }
+	@Override public ArrayUnweightedNode[] nodes() { return super.nodes(); }
+	@Override public ArrayUnweightedNode[] reverseNodes() { return super.reverseNodes(); }
 }
 class ArrayWeightedGraph extends WeightedGraph<ArrayWeightedNode> {
 	public ArrayWeightedGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	@Override protected final ArrayWeightedNode createNode(final int id) { return new ArrayWeightedNode(id); }
 	@Override protected final ArrayWeightedNode[] createNodeList(final int size) { return new ArrayWeightedNode[size]; }
+	@Override public ArrayWeightedNode edges() { return super.edges(); }
+	@Override public ArrayWeightedNode[] nodes() { return super.nodes(); }
+	@Override public ArrayWeightedNode[] reverseNodes() { return super.reverseNodes(); }
 }
 class ArrayTemplateGraph<T extends Comparable<T>> extends TemplateGraph<T, ArrayTemplateNode<T>> {
 	public ArrayTemplateGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	@Override protected final ArrayTemplateNode<T> createNode(final int id) { return new ArrayTemplateNode<T>(id); }
 	@Override @SuppressWarnings("unchecked") protected final ArrayTemplateNode<T>[] createNodeList(final int size) { return new ArrayTemplateNode[size]; }
+	@Override public ArrayTemplateNode<T> edges() { return super.edges(); }
+	@Override public ArrayTemplateNode<T>[] nodes() { return super.nodes(); }
+	@Override public ArrayTemplateNode<T>[] reverseNodes() { return super.reverseNodes(); }
 }
 class HashUnweightedGraph extends UnweightedGraph<HashUnweightedNode> {
 	public HashUnweightedGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	@Override protected final HashUnweightedNode createNode(final int id) { return new HashUnweightedNode(id); }
 	@Override protected final HashUnweightedNode[] createNodeList(final int size) { return new HashUnweightedNode[size]; }
+	@Override public HashUnweightedNode edges() { return super.edges(); }
+	@Override public HashUnweightedNode[] nodes() { return super.nodes(); }
+	@Override public HashUnweightedNode[] reverseNodes() { return super.reverseNodes(); }
 }
 class HashWeightedGraph extends WeightedGraph<HashWeightedNode> {
 	public HashWeightedGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	@Override protected final HashWeightedNode createNode(final int id) { return new HashWeightedNode(id); }
 	@Override protected final HashWeightedNode[] createNodeList(final int size) { return new HashWeightedNode[size]; }
+	@Override public HashWeightedNode edges() { return super.edges(); }
+	@Override public HashWeightedNode[] nodes() { return super.nodes(); }
+	@Override public HashWeightedNode[] reverseNodes() { return super.reverseNodes(); }
 }
 class HashTemplateGraph<T extends Comparable<T>> extends TemplateGraph<T, HashTemplateNode<T>> {
 	public HashTemplateGraph(final int numNode, final boolean directed) { super(numNode, directed); }
 	@Override protected final HashTemplateNode<T> createNode(final int id) { return new HashTemplateNode<T>(id); }
 	@Override @SuppressWarnings("unchecked") protected final HashTemplateNode<T>[] createNodeList(final int size) { return new HashTemplateNode[size]; }
+	@Override public HashTemplateNode<T> edges() { return super.edges(); }
+	@Override public HashTemplateNode<T>[] nodes() { return super.nodes(); }
+	@Override public HashTemplateNode<T>[] reverseNodes() { return super.reverseNodes(); }
 }
 
 interface AbstractNode<Edge extends AbstractEdge<Edge>> extends Collection<Edge> {  }
-class ArrayNode<Edge extends AbstractEdge<Edge>> extends ArrayList<Edge> implements AbstractNode<Edge> {
-	public final int id;
-	public ArrayNode(final int id) { this.id = id; }
-}
-class ArrayUnweightedNode extends ArrayNode<UnweightedEdge> { public ArrayUnweightedNode(final int id) { super(id); } }
-class ArrayWeightedNode extends ArrayNode<WeightedEdge> { public ArrayWeightedNode(final int id) { super(id); } }
-class ArrayTemplateNode<T extends Comparable<T>> extends ArrayNode<TemplateEdge<T>> { public ArrayTemplateNode(final int id) { super(id); } }
-class HashNode<Edge extends AbstractEdge<Edge>> extends HashSet<Edge> implements AbstractNode<Edge> {
-	public final int id;
-	public HashNode(final int id) { this.id = id; }
-}
-class HashUnweightedNode extends HashNode<UnweightedEdge> { public HashUnweightedNode(final int id) { super(id); } }
-class HashWeightedNode extends HashNode<WeightedEdge> { public HashWeightedNode(final int id) { super(id); } }
-class HashTemplateNode<T extends Comparable<T>> extends HashNode<TemplateEdge<T>> { public HashTemplateNode(final int id) { super(id); } }
+interface UnweightedNode extends AbstractNode<UnweightedEdge> {  }
+interface WeightedNode extends AbstractNode<WeightedEdge> {  }
+interface TemplateNode<T extends Comparable<T>> extends AbstractNode<TemplateEdge<T>> {  }
+class ArrayUnweightedNode extends ArrayList<UnweightedEdge> implements UnweightedNode { public final int id; public ArrayUnweightedNode(final int id) { this.id = id; } }
+class ArrayWeightedNode extends ArrayList<WeightedEdge> implements WeightedNode { public final int id; public ArrayWeightedNode(final int id) { this.id = id; } }
+class ArrayTemplateNode<T extends Comparable<T>> extends ArrayList<TemplateEdge<T>> implements TemplateNode<T> { public final int id; public ArrayTemplateNode(final int id) { this.id = id; } }
+class HashUnweightedNode extends HashSet<UnweightedEdge> implements UnweightedNode { public final int id; public HashUnweightedNode(final int id) { this.id = id; } }
+class HashWeightedNode extends HashSet<WeightedEdge> implements WeightedNode { public final int id; public HashWeightedNode(final int id) { this.id = id; } }
+class HashTemplateNode<T extends Comparable<T>> extends HashSet<TemplateEdge<T>> implements TemplateNode<T> { public final int id; public HashTemplateNode(final int id) { this.id = id; } }
 
 abstract class AbstractEdge<Edge extends AbstractEdge> {
 	public int source, target;
