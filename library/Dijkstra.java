d34e38dbddab66f47226d4bee77fe462402a4f92
shun0923 <shuny0923@gmail.com>
2022-10-01 13:29:59 +0900

    Update

--- library/Dijkstra.java
+++ library/Dijkstra.java
@@ -2,7 +2,7 @@ package library;
 
 import java.util.*;
 import library.SimpleUtil;
-import library.Graph;
+import library.AbstractGraph;
 import library.DistCalc;
 
 final class Dijkstra extends DistCalc {
