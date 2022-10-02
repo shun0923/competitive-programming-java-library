52e31ba92ece7941062041f7a06bde3811726113
shun0923 <shuny0923@gmail.com>
2022-10-02 20:17:54 +0900

    Create Graph reversed test

---
+++ library/Dijkstra_reversed_test.java
@@ -0,0 +1,30 @@
+// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2005
+
+package library;
+
+import library.Solver;
+import library.AbstractGraph;
+import library.Dijkstra;
+
+public class Dijkstra_reversed_test extends Solver {
+	public static void main(final String[] args) { main(args, new Dijkstra_reversed_test()); }
+
+	public void solve() {
+		while(true) {
+			int n = ni();
+			int m = ni();
+			int s = ni() - 1;
+			int g1 = ni() - 1;
+			int g2 = ni() - 1;
+			if(n == 0) return;
+			var g = new ArrayWeightedGraph(n, true);
+			for(int i = 0; i < m; i ++) g.add(ni() - 1, ni() - 1, nl());
+			long distS[] = Dijkstra.dist(g, s);
+			long distG1[] = Dijkstra.dist(g.numNode, g.reverseNodes(), g1);
+			long distG2[] = Dijkstra.dist(g.numNode, g.reverseNodes(), g2);
+			long min = INF;
+			for(int i = 0; i < n; i ++) min = min(min, distS[i] + distG1[i] + distG2[i]);
+			prtln(min);
+		}
+	}
+}
---
+++ library/WarshallFloyd_reversed_test.java
@@ -0,0 +1,31 @@
+// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2005
+
+package library;
+
+import library.Solver;
+import library.AbstractGraph;
+import library.WarshallFloyd;
+
+public class WarshallFloyd_reversed_test extends Solver {
+	public static void main(final String[] args) { main(args, new WarshallFloyd_reversed_test()); }
+
+	public void solve() {
+		while(true) {
+			int n = ni();
+			int m = ni();
+			int s = ni() - 1;
+			int g1 = ni() - 1;
+			int g2 = ni() - 1;
+			if(n == 0) return;
+			var g = new ArrayWeightedGraph(n, true);
+			for(int i = 0; i < m; i ++) g.add(ni() - 1, ni() - 1, nl());
+			long distS[] = WarshallFloyd.dist(g)[s];
+			long distReversed[][] = WarshallFloyd.dist(g.numNode, g.directed, g.reverseEdges());
+			long distG1[] = distReversed[g1];
+			long distG2[] = distReversed[g2];
+			long min = INF;
+			for(int i = 0; i < n; i ++) min = min(min, distS[i] + distG1[i] + distG2[i]);
+			prtln(min);
+		}
+	}
+}
