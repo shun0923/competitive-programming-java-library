693ed873e770d83e84f410399655c36f8316e117
shun0923 <shuny0923@gmail.com>
2022-10-04 10:50:43 +0900

    Update

--- library/Solver.java
+++ library/Solver.java
@@ -6,7 +6,7 @@ abstract class Solver extends Util implements Runnable {
 	public static void main(final String[] args, Runnable solver) {
 		DEBUG = args.length > 0 && args[0].equals("-DEBUG");
 		Thread.setDefaultUncaughtExceptionHandler((t, e) -> { flush(); e.printStackTrace(); System.exit(1); });
-		new Thread(null, solver, "", 1 << 31).start();
+		new Thread(null, solver, "", 1l << 31).start();
 	}
 	@Override
 	public void run() { solve(); flush(); }
