81ff674731930a5a396f51a5804c6487ee891687
shun0923 <shuny0923@gmail.com>
2022-10-05 13:27:59 +0900

    Update

--- library/TopologicalSort_test.java
+++ library/TopologicalSort_test.java
@@ -1,4 +1,4 @@
-// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_B
+// verification-helper: PROBLEM https://yukicoder.me/problems/no/468
 
 package library;
 
@@ -12,10 +12,26 @@ public class TopologicalSort_test extends Solver {
 	public void solve() {
 		int n = ni();
 		int m = ni();
-		ArrayUnweightedGraph g = new ArrayUnweightedGraph(n, true);
-		for(int i = 0; i < m; i ++) g.add(n - ni() - 1, n - ni() - 1);
-		int sorted[] = TopologicalSort.sort(g);
-		for(int i = 0; i < n; i ++) sorted[i] = n - sorted[i] - 1;
-		prtlns(sorted);
+		ArrayWeightedGraph g = new ArrayWeightedGraph(n, true);
+		for(int i = 0; i < m; i ++) g.add(ni(), ni(), nl());
+		long dist1[] = cal(n, g.nodes(), g.edges());
+		long dist2[] = cal(n, g.reverseNodes(), g.reverseEdges());
+		for(int i = 0; i < n; i ++) dist2[i] = dist1[0] - dist2[i];
+		int cnt = 0;
+		for(int i = 0; i < n; i ++) if(dist1[i] != dist2[i]) cnt ++;
+		prtln(dist1[0]+" "+cnt+"/"+n);
+	}
+
+	long[] cal(int n, WeightedNode[] nodes, WeightedNode edges) {
+		int sorted[] = TopologicalSort.sort(nodes, edges);
+		reverse(sorted);
+		long dist[] = new long[n];
+		dist[sorted[0]] = 0;
+		for(int i : sorted) {
+			for(WeightedEdge e : nodes[i]) {
+				dist[i] = max(dist[i], dist[e.target] + e.cost);
+			}
+		}
+		return dist;
 	}
 }
