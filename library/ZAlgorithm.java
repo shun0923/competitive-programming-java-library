2b1b842d80d9f42fcd12bea9371a0fae480355e4
shun0923 <shuny0923@gmail.com>
2022-10-05 16:39:43 +0900

    Create ZAlgorithm

---
+++ library/ZAlgorithm.java
@@ -0,0 +1,27 @@
+package library;
+
+import library.SimpleUtil;
+
+final class ZAlgolithm {
+	// return the length of the longest list starting from S[i] which is also a prefix of S
+	// O(|S|)
+	public static final int[] cal(String s) { return cal(s.toCharArray()); }
+	public static final int[] cal(char[] c) { return cal(SimpleUtil.charToInt(c)); }
+	public static final int[] cal(int[] a) {
+		int len = a.length;
+		int match[] = new int[len];
+		match[0] = len;
+		int i = 1;
+		int j = 0;
+		while(i < len) {
+			while(i + j < len && a[j] == a[i + j]) j ++;
+			match[i] = j;
+			if(j == 0) { i ++; continue; }
+			int k = 1;
+			while(i + k < len && k + match[k] < j) { match[i + k] = match[k]; k ++; }
+			i += k;
+			j -= k;
+		}
+		return match;
+	}
+}
---
+++ library/ZAlgorithm_test.java
@@ -0,0 +1,14 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/zalgorithm
+
+package library;
+
+import library.Solver;
+import library.ZAlgorithm;
+
+public class ZAlgorithm_test extends Solver {
+	public static void main(final String[] args) { main(args, new ZAlgorithm_test()); }
+
+	public void solve() {
+		prtln(ZAlgolithm.cal(ns()));
+	}
+}
