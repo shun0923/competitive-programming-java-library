5f3c2d5e01764ef45757323003cb09c8e39a19f7
shun0923 <shuny0923@gmail.com>
2022-10-31 15:37:50 +0900

    Update

--- library/ArbitraryMod_pow_test.java
+++ library/ArbitraryMod_pow_test.java
@@ -5,8 +5,8 @@ package library;
 import library.Solver;
 import library.ArbitraryMod;
 
-public class ArbitraryModPow_test extends Solver {
-	public static void main(final String[] args) { main(args, new ArbitraryModPow_test()); }
+public class ArbitraryMod_pow_test extends Solver {
+	public static void main(final String[] args) { main(args, new ArbitraryMod_pow_test()); }
 
 	public void solve() {
 		prtln(new ArbitraryMod(1_000_000_007).pow(nl(), nl()));
